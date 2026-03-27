de97d5e3d65f67a9b5e02825023c8d0e514865d3
Miroslav Bajtoš <oss@bajtos.net>
2026-03-27 15:07:54 +0100

    Use the new S3 URL for production (#117)
    * Use the new S3 URL for production
    
    https://eu-west-1.s3.fil.one
    
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>
    
    * Update S3 URL in the frontend
    
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>
    
    * fixup! update code comment
    
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>
    
    ---------
    
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>

--- sst.config.ts
+++ sst.config.ts
@@ -303,10 +303,10 @@ export default $config({
       AURORA_REGION_ID: 'ff',
     };
 
-    // TODO: switch to https://eu-west-1.s3.fil.one (production) and https://eu-west-1.s3.staging.fil.one (non-prod).
+    // TODO: once https://eu-west-1.s3.staging.fil.one is live, switch the staging URL as well
     // https://github.com/filecoin-project/fil-one/pull/111
     const auroraS3GatewayUrl = isProduction
-      ? 'https://a-fil-one-s3.aur.lu'
+      ? 'https://eu-west-1.s3.fil.one'
       : 'https://s3.dev.aur.lu';
 
     const auroraApiKeySsmArn = $interpolate`arn:aws:ssm:*:*:parameter/filone/${$app.stage}/aurora-portal/tenant-api-key/*`;
