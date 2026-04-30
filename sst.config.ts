be70b41ee8317e46928a2327fa428cd4c945035e
Joe Muoio <joegm258@gmail.com>
2026-04-29 17:01:42 -0700

    version bump on setup lambda to force deploy of enrollment trigger

--- sst.config.ts
+++ sst.config.ts
@@ -323,7 +323,7 @@ export default $config({
               ServiceToken: setupFn.arn,
               SiteUrl: siteUrl,
               Stage: $app.stage,
-              Version: '2.3',
+              Version: '2.4',
             },
           },
         },
